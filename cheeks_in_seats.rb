5a88a428b72515975e6a6e1c67e906d3cb2a3e9f
Sarah Tatro <statrox4@gmail.com>
2019-09-05 15:16:36 -0600

    Add additional requirements & set environment variable for ticketmaster api key
    Co-Authored-By: null <GlynnisOC@users.noreply.github.com>
    Co-Authored-By: S. Mainar <smainar@users.noreply.github.com>
    Co-Authored-By: Aurie <Myrdden@users.noreply.github.com>

--- cheeks_in_seats.rb
+++ cheeks_in_seats.rb
@@ -1,7 +1,12 @@
-require 'sinatra/bundler'
+require 'bundler/setup'
+require 'sinatra/base'
+require './secretz.rb'
 
 ENV['SINATRA_ENV'] ||= 'development'
+ENV['TICKETMASTER-API-KEY'] = SECRET_TICKETMASTER
 
 Bundler.require :default, ENV['SINATRA_ENV']
+
+
 
 require_all './app'
