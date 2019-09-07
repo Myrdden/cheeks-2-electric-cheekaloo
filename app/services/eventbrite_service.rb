351892b347677e993a6cca7cae9c338af892ac3b
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-06 20:52:17 -0600

    Add connections for the different api calls needed to eventbrite api

--- app/services/eventbrite_service.rb
+++ app/services/eventbrite_service.rb
@@ -1,13 +1,49 @@
 class EventbriteService
 
   def events
+    event_info
+  end
+
+  # def venues
+  #   venue_info
+  # end
+  #
+  # def tickets
+  #   ticket_info
+  # end
+  #
+  # def genre
+  #   genre_info
+  # end
+
+  private
+  def event_info
     response = Faraday.get("https://www.eventbriteapi.com/v3/events/search") do |request|
       request.headers['Authorization'] = 'Bearer ' + ENV['EVENTBRITE-API-KEY']
     end
-    JSON.parse()
+    JSON.parse(response.body, symbolize_names: true)[:events]
   end
 
-  def venues
+  def venue_info
+    response = Faraday.get("https://www.eventbriteapi.com/v3/events/#{event_id}/") do |request|
+      request.headers['Authorization'] = 'Bearer ' + ENV['EVENTBRITE-API-KEY']
+      request.params['expand'] = "venue"
+    end
+    JSON.parse(response.body, symbolize_names: true)[:venue]
   end
 
+  def ticket_info
+    response = Faraday.get("https://www.eventbriteapi.com/v3/events/#{event_id}") do |request|
+      request.headers['Authorization'] = 'Bearer ' + ENV['EVENTBRITE-API-KEY']
+      request.params['expand'] = "ticket_classes"
+    end
+    JSON.parse(response.body, symbolize_names: true)[:ticket_classes]
+  end
+
+  def genre_info
+    response = Faraday.get("https://www.eventbriteapi.com/v3/categories/") do |request|
+      request.headers['Authorization'] = 'Bearer ' + ENV['EVENTBRITE-API-KEY']
+    end
+    JSON.parse(response.body, symbolize_names: true)[:categories]
+  end
 end
